2c74c2a3a9952f70d8c8a29c1f5455251fb20b82
Johannes Endres <je@johannes-endres.de>
2022-12-31 21:30:11 +0100

    make demo

---
+++ background.js
@@ -0,0 +1,16 @@
+messenger.composeAction.onClicked.addListener(async (tab, info) => {
+    // Get the current body text
+    let details = await messenger.compose.getComposeDetails(tab.id);
+    // Replace it with the empty string
+    details.plainTextBody = "";
+    // Send the empty message body
+    await messenger.compose.setComposeDetails(tab.id, details);
+    // Check if the message body is empty by dumping it to the console
+    details = await messenger.compose.getComposeDetails(tab.id);
+    console.debug(details.plainTextBody);
+});
+
+messenger.browserAction.onClicked.addListener(() => messenger.compose.beginNew({
+    isPlainText: true,
+    plainTextBody: "Lorem ipsum dolor sit amet"
+}))
